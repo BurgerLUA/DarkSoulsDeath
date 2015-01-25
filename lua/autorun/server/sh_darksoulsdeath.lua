1688cb28a78b67c77feba833a7bfcb83877b4a24
BurgerLUA <mattwasnothere@gmail.com>
2015-01-25 15:33:45 -0800

    Optimizations

---
+++ lua/autorun/server/sh_darksoulsdeath.lua
@@ -0,0 +1,27 @@
+
+if SERVER then
+	function OverrideDeathSound()
+		return true
+	end
+	
+	hook.Add("PlayerDeathSound", "OverrideDeathSound", OverrideDeathSound)
+end
+
+--[[
+
+[media]http://www.youtube.com/watch?v=JAWS-Ctv0dw[/media]
+
+[quote]
+Dark Souls Death Message for Garrysmod sounds ripped from Dark Souls, textures custom made. 
+
+Dark Souls was developed by FromSoftware, Published by BANDAI NAMCO Games. 
+
+If you install this on your server, players who do not have this addon installed will not be able to see or hear it it unless you use resource.AddWorkshop or tell your clients to install this addon. 
+
+I am aware that there is another Dark Souls Death Message on the workshop, this is not a copy or a reupload of that addon. If you need proof, you can view the code on github [URL="https://github.com/BurgerLUA/DarkSoulsDeath"]here[/URL]
+[/quote]
+
+[URL="http://steamcommunity.com/sharedfiles/filedetails/?id=373858511"][img]http://i.imgur.com/S7kzKcc.png[/img][/URL]
+
+--]]
+
